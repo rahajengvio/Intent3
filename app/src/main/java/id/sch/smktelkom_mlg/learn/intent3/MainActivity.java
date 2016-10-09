63858ac205ab252680e2b04c736915e32bd04e33
Rahajeng <rahajeng_violita_23rpl@student.smktelkom-mlg.sch.id>
2016-10-09 15:50:45 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageViewPhone)
+                .setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        dialPhoneNumber("0341712500");
+                    }
+
+                });
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        composeSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+                });
+    }
+
+    private void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body",message);
+        if(intent.resolveActivity(getPackageManager())!=null)
+        startActivity(intent);
+    }
+
+    private void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"+phoneNumber));
+        if(intent.resolveActivity(getPackageManager())!=null)
+            startActivity(intent);
     }
 }
